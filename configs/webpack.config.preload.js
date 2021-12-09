13a7545ef008fe188aced05ae4f685ac85212872
Amir <amir77mafi@gmail.com>
2021-12-09 21:15:12 +0330

    Use webpack target `electron-preload` for preloads

--- configs/webpack.config.preload.js
+++ configs/webpack.config.preload.js
@@ -2,8 +2,9 @@ const { merge } = require('webpack-merge');
 const BaseConfig = require('./webpack.config.base');
 
 module.exports = merge(BaseConfig, {
-  target: 'electron-main',
+  target: 'electron-preload',
   entry: {
-    main: './src/main/main.js',
+    'home.preload': './src/renderer/home/preload.js',
+    'popup.preload': './src/renderer/popup/preload.js',
   },
 });
